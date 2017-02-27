f7d6b77fad9ea786299133a62e26af0bd6a50fe6
Nathan Foote <nfoote.alt@gmail.com>
2017-02-27 17:45:26 -0500

    listen

---
+++ listen.js
@@ -0,0 +1,17 @@
+var sys = require('sys');
+var exec = require('child_process').exec;
+function react(error, stdout, stderr){
+	if (stdout.indexOf('origin/master") >= 0) {
+		exec("cd ~/chicken.io && sudo forever restart app.js");
+	}
+}
+var http = require('http');
+var sleep = require('sleep');
+
+http.createServer(function (require, response){
+	exec("cd ~/chicken.io && git pull" , react);
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.write('Pulling...');
+	response.end();
+	sleep.sleep(10);
+}).listen(2000);
